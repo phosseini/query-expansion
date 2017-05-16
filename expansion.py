38979c821b170d93f7989c358d6a3575fefe6557
Pedram <pdr.hosseini@gmail.com>
2017-05-16 16:02:55 -0400

    updating the pipeline

--- expansion.py
+++ expansion.py
@@ -1,39 +1,69 @@
 import itertools
 import nltk
-import expansion
 from nltk.corpus import wordnet
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
 
+def run(queryList):
 
-def run(query):
-    # tokenizing the query
-    tokens = nltk.word_tokenize(query)
-
-    synonyms = []  # synonyms of all the tokens
-
-    # iterating through the tokens
-    for item in tokens:
-        synsets = wordnet.synsets(item)
-
-        # synonyms of the current token
-        currentSynonyms = []
-
-        # iterating through the synsets
-        for i in synsets:
-            if i.lemmas()[0].name() not in currentSynonyms:  # if we have not
-                # print(i.lemmas()[0].name().replace("_", " "))
-                currentSynonyms.append(i.lemmas()[0].name().replace("_", " "))
-
-        synonyms.append(currentSynonyms)
-
+    stemmer = PorterStemmer()
     f = open("data/expanded.txt", "w+")
-    f.write(query)
-    f.write(", ")
-    # now that we have all the synonyms
-    for x in itertools.product(*synonyms):
-        current = ""
-        for item in x:
-            current += item
-            current += " "
-        current += ","
-        f.write(current)
+    for query in queryList:
+        querySplitted = query.split(",")
 
+        # tokenizing the query
+        tokens = nltk.word_tokenize(querySplitted[1])
+
+        # stemming the tokens in the query
+        stemmed_tokens = [stemmer.stem(i) for i in tokens]
+
+        # removing stop words in the query
+        filtered_words = [word for word in stemmed_tokens if word not in stopwords.words('english')]
+
+        # pos tagging of tokens
+        pos = nltk.pos_tag(filtered_words)
+
+        synonyms = []  # synonyms of all the tokens
+
+        index = 0
+        # iterating through the tokens
+        for item in filtered_words:
+            synsets = wordnet.synsets(item)
+
+            # synonyms of the current token
+            currentSynonyms = []
+            currentPOS = get_wordnet_pos(pos[index])
+
+            # iterating through the synsets
+            for i in synsets:
+                # first we check if token and synset have the same part of speech
+                if str(i.pos()) == str(currentPOS):
+                    for j in i.lemmas():
+                        if j.name() not in currentSynonyms:  # if we have not
+                            currentSynonyms.append(j.name().replace("_", " "))
+                synonyms.append(currentSynonyms)
+
+        f.write(querySplitted[0] + ", " + querySplitted[1] + ", ")
+        # now that we have all the synonyms
+        for x in itertools.product(*synonyms):
+            current = ""
+            for item in x:
+                current += item
+                current += " "
+            current += ", "
+            f.write(current)
+        f.write("\n")
+
+
+def get_wordnet_pos(treebank_tag):
+
+    if treebank_tag[1].startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag[1].startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag[1].startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag[1].startswith('R'):
+        return wordnet.ADV
+    else:
+        return ''
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import expansion
+import xlrd
+
+
+queryList = []
+
+workbook = xlrd.open_workbook('data/Mapping.xls')
+sheet_names = workbook.sheet_names()
+xl_sheet = workbook.sheet_by_name(sheet_names[0])
+
+for row_idx in range(0, xl_sheet.nrows):    # Iterate through rows
+        cell_id = xl_sheet.cell(row_idx, 0).value  # Get id cell
+        cell_topic = xl_sheet.cell(row_idx, 2).value  # Get topic cell
+        queryList.append(str(cell_id) + "," + str(cell_topic))
 
 # the query that we want to expand
-query = "program chair"
-expansion.run(query)
+expansion.run(queryList)
